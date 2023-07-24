70149a1b51fc90008594168a5bc71679921e7786
ariellouise <60407963+ariellouise@users.noreply.github.com>
2023-07-24 12:44:43 -0400

    Initial commit

---
+++ W4/assignment2-2.py
@@ -0,0 +1,9 @@
+# This first line is provided for you
+
+# This first line is provided for you
+hrs = input("Enter Hours:")
+rte = input("Enter Rate Per Hour:")
+hours = float(hrs)
+rate = float(rte)
+
+print ("Pay:",hours * rate)
